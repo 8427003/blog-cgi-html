1686183a057d80d07c5205671ccff2e2a156afd5
8427003 <8427003@qq.com>
2014-11-16 01:43:53 -0500

    fsdf

--- cgi-bin/statichtml.py
+++ cgi-bin/statichtml.py
@@ -31,7 +31,7 @@ def staticHtml(art):
     t = Template(txt)
 
     result = t.safe_substitute(title=art["title"],content=art["content"])
-    path = './Cloris/article/c%s/'%article["cid"]
+    path = './8427003\.github\.io/article/c%s/'%article["cid"]
     if not os.path.exists(path):
         os.makedirs(path)
 
